110df0fc5f74da1bb4bf45368dbdd198fadf2ca5
lautaromateol <lautaromateol@gmail.com>
2025-05-29 00:36:54 -0300

    add: users authentication services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,89 @@
 // import bcrypt from 'bcryptjs';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUser, CreateUserObject } from './types/create-user';
+import { SignInUser, SignInUserObject } from './types/sign-in-user';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signInUser(user: SignInUser) {
+    const validation = SignInUserObject.safeParse(user);
+
+    if (!validation.success) {
+      throw new BadRequestException({
+        errors: validation.error.issues,
+        statusCode: 400,
+      });
+    }
+
+    const isUserRegistered = await this.findUserByEmail(user.email);
+
+    if (!isUserRegistered) {
+      throw new UnauthorizedException(
+        'The email or the password are incorrect.',
+      );
+    }
+
+    const isUserAuthorized = await this.authorizeUser({
+      email: user.email,
+      password: user.password,
+    });
+
+    if (!isUserAuthorized) {
+      throw new UnauthorizedException(
+        'The email or the password are incorrect.',
+      );
+    }
+
+    const token = await this.authenticateUser(isUserAuthorized);
+
+    return {
+      success: true,
+      token,
+      statusCode: 200,
+    };
+  }
+
+  async authenticateUser(user: User) {
+    const payload = { sub: user.id, email: user.email };
+
+    const token = await this.jwtService.signAsync(payload, {
+      secret: process.env.JWT_SECRET,
+    });
+
+    return token;
+  }
+
+  async authorizeUser(user: SignInUser) {
+    const dbUser = await this.prismaService.user.findUnique({
+      where: {
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    return dbUser;
+  }
 
   async createUser(user: CreateUser) {
     const validation = CreateUserObject.safeParse(user);
 
     if (!validation.success) {
-      throw new BadRequestException({ errors: validation.error.issues, statusCode: 400 });
+      throw new BadRequestException({
+        errors: validation.error.issues,
+        statusCode: 400,
+      });
     }
 
     const userWithEmail = await this.prismaService.user.findUnique({
