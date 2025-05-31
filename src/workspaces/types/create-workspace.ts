5320540d03746828ff3311f62ee0dfbab3bd538f
lautaromateol <lautaromateol@gmail.com>
2025-05-30 23:25:48 -0300

    add: workspaces creation schema

---
+++ src/workspaces/types/create-workspace.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const CreateWorkspaceObject = z.object({
+  userId: z.string().uuid(),
+  name: z
+    .string()
+    .min(5, {
+      message: 'Please insert at least 5 characters',
+    })
+    .max(50, {
+      message: 'The maximum length allowed it is 50 characters.',
+    }),
+});
+
+export type CreateWorkspace = z.infer<typeof CreateWorkspaceObject>
