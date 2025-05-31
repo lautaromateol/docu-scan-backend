7570ced55f66a1c895efcea925428b022a117fa1
lautaromateol <lautaromateol@gmail.com>
2025-05-30 23:25:04 -0300

    add: workspaces and members modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, WorkspacesModule, MembersModule],
   controllers: [AppController],
   providers: [AppService],
 })
