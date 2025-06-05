974f4ab167231220516168e53382f7e3963ff826
lautaromateol <lautaromateol@gmail.com>
2025-06-05 16:29:50 -0300

    add: auth guard

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const authorization = request.headers.authorization;
+    const token = authorization?.split(' ')[1];
+
+    if (!authorization) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const tokenPayload = await this.jwtService.verifyAsync(token);
+      request.user = {
+        id: tokenPayload.sub,
+      };
+
+      return true;
+    } catch (error) {
+      console.log(error)
+      throw new UnauthorizedException();
+    }
+  }
+}
