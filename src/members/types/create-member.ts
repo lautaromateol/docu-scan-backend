dec522012b56a1b167a0b815edc28c8dc89e6ed9
lautaromateol <lautaromateol@gmail.com>
2025-06-05 18:33:06 -0300

    update: create member schema

--- src/members/types/create-member.ts
+++ src/members/types/create-member.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const CreateMemberObject = z.object({
   userId: z.string().uuid(),
-  workspaceId: z.string().uuid()
+  workspaceId: z.string().uuid(),
+  role: z.enum(["MEMBER", "ADMIN"])
 })
 
 export type CreateMember = z.infer<typeof CreateMemberObject>
