dfd163185e1e6ec256b2075ee399b29024ccfc85
lautaromateol <lautaromateol@gmail.com>
2025-06-05 16:35:34 -0300

    add: user info GET route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,24 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @UseGuards(AuthGuard)
+  @Get('user-info')
+  getUser(@Request() request) {
+    return this.usersService.getUser(request.user.id);
+  }
 
   @Delete(':id')
   deleteUser(@Param('id') id: string) {
