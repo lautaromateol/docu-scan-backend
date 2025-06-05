2ebdb90fc758afdc5c7a28c704294f14affeff29
lautaromateol <lautaromateol@gmail.com>
2025-06-05 18:31:58 -0300

    add: workspace update service

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,19 +1,73 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { MembersService } from '../members/members.service';
 import {
   CreateWorkspace,
   CreateWorkspaceObject,
 } from './types/create-workspace';
+import {
+  UpdateWorkspace,
+  UpdateWorkspaceObject,
+} from './types/update-workspace';
 
 @Injectable()
 export class WorkspacesService {
   constructor(
     private prismaService: PrismaService,
-    private membersService: MembersService
+    private membersService: MembersService,
   ) {}
 
-  async createWorkspace(workspace: CreateWorkspace) {
+  async updateWorkspace({
+    workspace,
+    workspaceId,
+    userId,
+  }: {
+    workspace: UpdateWorkspace;
+    workspaceId: string;
+    userId: string;
+  }) {
+    const validation = UpdateWorkspaceObject.safeParse(workspace);
+
+    if (!validation.success) {
+      throw new BadRequestException({
+        errors: validation.error.issues,
+        statusCode: 400,
+      });
+    }
+
+    const member = await this.membersService.getMember(userId);
+
+    if (!member || member.role !== 'ADMIN') {
+      throw new UnauthorizedException();
+    }
+
+    const dbWorkspace = await this.prismaService.workspace.update({
+      where: {
+        id: workspaceId,
+      },
+      data: {
+        ...workspace,
+      },
+    });
+
+    return {
+      success: true,
+      workspace: dbWorkspace,
+      statusCode: 200,
+    };
+  }
+
+  async createWorkspace({
+    workspace,
+    userId,
+  }: {
+    userId: string;
+    workspace: CreateWorkspace;
+  }) {
     const validation = CreateWorkspaceObject.safeParse(workspace);
 
     if (!validation.success) {
@@ -25,16 +79,20 @@ export class WorkspacesService {
 
     const dbWorkspace = await this.prismaService.workspace.create({
       data: {
-        name: workspace.name
-      }
-    })
+        name: workspace.name,
+      },
+    });
 
-    await this.membersService.createMember({ userId: workspace.userId, workspaceId: dbWorkspace.id })
+    await this.membersService.createMember({
+      userId,
+      workspaceId: dbWorkspace.id,
+      role: 'ADMIN',
+    });
 
     return {
       success: true,
       workspace: dbWorkspace,
-      statusCode: 200
-    }
+      statusCode: 200,
+    };
   }
 }
