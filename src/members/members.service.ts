56bc3e2d2a1323890d66600a1b24c83fcf0bc11a
lautaromateol <lautaromateol@gmail.com>
2025-06-08 18:29:54 -0300

    update: workspaceId param added to getMember service

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,37 +1,43 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
-import { PrismaService } from "../prisma/prisma.service";
-import { CreateMember, CreateMemberObject } from "./types/create-member";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateMember, CreateMemberObject } from './types/create-member';
 
 @Injectable()
 export class MembersService {
   constructor(private prismaService: PrismaService) {}
 
-  async getMember(userId: string) {
+  async getMember({
+    workspaceId,
+    userId,
+  }: {
+    workspaceId: string;
+    userId: string;
+  }) {
     const member = await this.prismaService.member.findFirst({
-      where: { userId }
-    })
+      where: { userId, workspaceId },
+    });
 
-    return member
+    return member;
   }
 
   async createMember(member: CreateMember) {
-    const validation = CreateMemberObject.safeParse(member)
+    const validation = CreateMemberObject.safeParse(member);
 
-    if(!validation.success) {
+    if (!validation.success) {
       throw new BadRequestException({
         errors: validation.error.issues,
         statusCode: 400,
-      })
+      });
     }
 
     const dbMember = await this.prismaService.member.create({
       data: {
         userId: member.userId,
         workspaceId: member.workspaceId,
-        role: member.role
-      }
-    })
+        role: member.role,
+      },
+    });
 
-    return dbMember
+    return dbMember;
   }
 }
