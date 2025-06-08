47a533522dcd6ab99662d0593f03e285954140be
lautaromateol <lautaromateol@gmail.com>
2025-06-08 18:41:05 -0300

    add: join workspace service

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,6 +1,7 @@
 import {
   BadRequestException,
   Injectable,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
@@ -22,6 +23,46 @@ export class WorkspacesService {
     private membersService: MembersService,
   ) {}
 
+  async joinWorkspace({
+    userId,
+    inviteCode,
+  }: {
+    userId: string;
+    inviteCode: string;
+  }) {
+
+    const workspace = await this.prismaService.workspace.findFirst({
+      where: {
+        inviteCode
+      }
+    })
+
+    if(!workspace || (workspace.inviteCode !== inviteCode)) {
+      throw new UnauthorizedException("This invite code is invalid.")
+    }
+    
+    const member = await this.membersService.getMember({ userId, workspaceId: workspace.id })
+
+    if(member) {
+      return {
+        success: true,
+        statusCode: 200,
+        message: "This user is already a workspace member."
+      }
+    }
+
+    await this.membersService.createMember({
+      role: 'MEMBER',
+      userId,
+      workspaceId: workspace.id,
+    });
+
+    return {
+      success: true, 
+      statusCode: 200
+    }
+  }
+
   async deleteWorkspace({
     workspaceId,
     userId,
@@ -29,21 +70,21 @@ export class WorkspacesService {
     workspaceId: string;
     userId: string;
   }) {
-    const member = await this.membersService.getMember(userId)
+    const member = await this.membersService.getMember({ workspaceId, userId });
 
-    if(!member || member.role !== "ADMIN") {
-      throw new UnauthorizedException()
+    if (!member || member.role !== 'ADMIN') {
+      throw new UnauthorizedException();
     }
 
     const deletedWorkspace = await this.prismaService.workspace.delete({
-      where: { id: workspaceId }
-    })
+      where: { id: workspaceId },
+    });
 
-    return { 
+    return {
       success: true,
       workspace: deletedWorkspace,
-      statusCode: 200
-     }
+      statusCode: 200,
+    };
   }
 
   async updateWorkspace({
@@ -64,7 +105,7 @@ export class WorkspacesService {
       });
     }
 
-    const member = await this.membersService.getMember(userId);
+    const member = await this.membersService.getMember({ workspaceId, userId });
 
     if (!member || member.role !== 'ADMIN') {
       throw new UnauthorizedException();
@@ -105,7 +146,7 @@ export class WorkspacesService {
     const dbWorkspace = await this.prismaService.workspace.create({
       data: {
         name: workspace.name,
-        inviteCode: generateInviteCode(10)
+        inviteCode: generateInviteCode(10),
       },
     });
 
