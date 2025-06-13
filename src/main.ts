8e2208923bd37e593e2214e36e9a482f8789d47b
lautaromateol <lautaromateol@gmail.com>
2025-06-13 00:52:51 -0300

    update: CORS implemented

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(urlencoded({ extended: true }));
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
