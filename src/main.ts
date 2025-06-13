a6a90e8aa3f8d2c67459235cb8d2517061fcdc77
lautaromateol <lautaromateol@gmail.com>
2025-06-13 00:55:02 -0300

    update: swagger implemented

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,14 @@ async function bootstrap() {
   app.enableCors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle("Docuscan API.")
+  .setVersion("1.0")
+  .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup("api", app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
