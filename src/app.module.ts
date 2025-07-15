5821fc31c03df523ac267788336b2f57dc0eaa35
lautaromateol <lautaromateol@gmail.com>
2025-07-15 14:59:29 -0300

    update: contracts module added to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { MembersModule } from './members/members.module';
+import { ContractsModule } from './contracts/contracts.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, WorkspacesModule, MembersModule],
+  imports: [PrismaModule, UsersModule, AuthModule, WorkspacesModule, MembersModule, ContractsModule],
   controllers: [AppController],
   providers: [AppService],
 })
