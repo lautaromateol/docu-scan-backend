c72d6bf2b1d526d573491dd44a0f94a7c988957e
lautaromateol <lautaromateol@gmail.com>
2025-07-17 15:33:16 -0300

    update: required modules added to contracts module

--- src/contracts/contracts.module.ts
+++ src/contracts/contracts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common"
 import { ContractsController } from "./contracts.controller";
 import { ContractsService } from "./contracts.service";
+import { PrismaModule } from "../prisma/prisma.module";
+import { MembersModule } from "../members/members.module";
 
 @Module({
+  imports: [PrismaModule, MembersModule],
   controllers: [ContractsController],
   providers: [ContractsService]
 })
