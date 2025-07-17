33ab770c4e0076a98c508590209465a6412a168f
lautaromateol <lautaromateol@gmail.com>
2025-07-17 15:31:45 -0300

    add: groq + llama config file

---
+++ config/llm.config.ts
@@ -0,0 +1,8 @@
+export const LLM_CONFIG = {
+  model: 'meta-llama/llama-4-scout-17b-16e-instruct',
+  temperature: 0.2,
+  max_completion_tokens: 2048,
+  top_p: 1,
+  stop: null,
+  stream: false, 
+}
